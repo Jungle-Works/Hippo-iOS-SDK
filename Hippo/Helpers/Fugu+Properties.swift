6fe1df1d30fcb9c2b24bf6aae3c342f21637cd4f
Arohi Magotra <arohi.magotra@jungleworks.com>
2020-10-20 21:48:58 +0530

    Version 2.1.41

--- Hippo/Helpers/Fugu+Properties.swift
+++ Hippo/Helpers/Fugu+Properties.swift
@@ -12,7 +12,7 @@ let HippoSDKSource = 1
 let Device_Type_iOS = 2
 var userDetailData = [String: Any]()
 
-let fuguAppVersion = "2.1.38"
+let fuguAppVersion = "2.1.41"
 var versionCode = 320
 
 /*
@@ -131,5 +131,5 @@ var versionCode = 320
  
  334 *** 2.1.35 = Bug fixes
 
- 334 *** 2.1.38 = Search user unique key issues
+ 334 *** 2.1.41 = Search user unique key issues
  */
