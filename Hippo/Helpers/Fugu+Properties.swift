1eba10c95d730cf704d880bcd7c0f13c65eec44e
rahulvermaJW <95697540+rahulvermaJW@users.noreply.github.com>
2021-12-29 17:01:53 +0530

    Updated

--- Hippo/Helpers/Fugu+Properties.swift
+++ Hippo/Helpers/Fugu+Properties.swift
@@ -12,7 +12,7 @@ let HippoSDKSource = 1
 let Device_Type_iOS = 2
 var userDetailData = [String: Any]()
 
-let fuguAppVersion = "2.1.58"
+let fuguAppVersion = "2.1.59"
 var versionCode = 250
 
 /*
@@ -164,5 +164,7 @@ var versionCode = 250
  357 *** 2.1.57 = Call dependency added
 
  358 *** 2.1.58 = Call dependency added and bugs fixed.
+ 
+ 359 *** 2.1.59 = ios 15 issues fixes
 
  */
