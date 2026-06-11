9fd499f8ca3871a4a5e71251dab60c9b68e338c2
Harpreet-singh <Harpreet.singh@jungleworks.com>
2026-06-11 16:29:48 +0530

    Added new parameter & coundoen time in join meeting

--- Hippo/Wrappers/FuguConfig/HippoConfig.swift
+++ Hippo/Wrappers/FuguConfig/HippoConfig.swift
@@ -802,8 +802,8 @@ struct WhatsappWidgetConfig{
         }
     }
     
-    public func joinVideoSdkCallByMeetingID(transaction_id: String, meetingDuration: TimeInterval = 0 * 60){
-        HippoCallClient.shared.joinCall(transaction_id: transaction_id, meetingDuration: meetingDuration)
+    public func joinVideoSdkCallByMeetingID(transaction_id: String, meetingDuration: TimeInterval = 0 * 60, name: String? = nil, startTime: String? = nil, endTime: String? = nil){
+        HippoCallClient.shared.joinCall(transaction_id: transaction_id, meetingDuration: meetingDuration, name: name, startTime: startTime, endTime: endTime)
     }
     
     public func endVideoCall(){
